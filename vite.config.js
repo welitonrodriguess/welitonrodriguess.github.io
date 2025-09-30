764f501ec02fac5f1bd991c067f59983b32befd3
Weliton Rodrigues <weliton.rodrigues290@icloud.com>
2025-09-30 19:41:48 -0300

    fix: base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If deploying to a GitHub project page, set VITE_BASE to '/<repo-name>/'
-// e.g., VITE_BASE=/portfolio
+
 const base = process.env.VITE_BASE || '/'
 
 export default defineConfig({
-  base,
+  base: './',
   plugins: [react()],
 })
